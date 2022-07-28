9656ae418a89a5f0cc7452d4152b672bd00788e8
leo <ionescu.leonard28@gmail.com>
2022-07-28 19:17:52 +0300

    added useQuery

--- pages/one.js
+++ pages/one.js
@@ -1,30 +1,27 @@
 import { React, useEffect, useState } from 'react'
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from 'axios'
 
 const one = () => {
-    const [loading, setLoading] = useState(false)
     const [imgSrc, setImgSrc] = useState('')
-    
+
     const getImage = async () => {
-        setLoading(true)
-        let response = await fetch('https://dog.ceo/api/breeds/image/random')
-        
-        response.json().then(response => {
-            if (response.status == 'success') {
-                setImgSrc(response.message)
-                setLoading(false)
-            }
-        })
+        let response = await axios.get('https://dog.ceo/api/breeds/image/random')
+        return response.data
     }
 
-    useEffect(() => {
-        getImage()
-    }, [])
+    let { data, status } = useQuery([], getImage)
+    
 
   return (
     <div>
-        {loading && <div>Loading...</div>}
-        {!loading && <img src={imgSrc} className=""></img>}          
-        {!loading && <button onClick={getImage} className="border-2 p-2 mt-4">Load another image</button>}
+        {status == 'loading' && <div>Loading...</div>}
+        {status == 'success' && <img src={data.message} className=""></img>}          
+          {status == 'success' && <button onClick={async e => {
+              data = { message: (await getImage()).message }
+              console.log(data)
+              console.log(status)
+          }} className="border-2 p-2 mt-4">Load another image</button>}
     </div>
   )
 }
